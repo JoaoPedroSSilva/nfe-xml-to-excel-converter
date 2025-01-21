4676f9cb42b5343326cafcf590e9f74674f90da9
JoaoPedroSSilva <97jpss@gmail.com>
2025-01-20 22:35:39 -0300

    Atualização back end colunas da tabela

--- back-end/NFeConverter/NFeConverter/Controllers/NFeController.cs
+++ back-end/NFeConverter/NFeConverter/Controllers/NFeController.cs
@@ -25,18 +25,42 @@ namespace NFeConverter.Controllers {
                 }
 
                 var dataTable = new DataTable();
-                dataTable.Columns.Add("ExemploColuna1");
-                dataTable.Columns.Add("ExemploColuna2");
+                dataTable.Columns.Add("Chave");
+                dataTable.Columns.Add("Valor Total");
+                dataTable.Columns.Add("Emitente");
+                dataTable.Columns.Add("Destinatário");
+                dataTable.Columns.Add("Produto");
+                dataTable.Columns.Add("Quantidade");
 
-                foreach (XmlNode node in doc.SelectNodes("//SeuXpathAqui")) {
+                Console.WriteLine(doc.SelectNodes("//nfeProc").Count);
+
+                foreach (XmlNode node in doc.SelectNodes("//nfeProc")) {
                     var row = dataTable.NewRow();
-                    row["ExemploColuna1"] = node["Elemento1"]?.InnerText;
-                    row["ExemploColuna2"] = node["Elemento2"]?.InnerText;
+                    row["Chave"] = node.Attributes["Id"]?.Value.Replace("NFe", ""); 
+                    row["Valor Total"] = node.SelectSingleNode("total/ICMSTot/vNF")?.InnerText;
+                    row["Emitente"] = node.SelectSingleNode("emit/xNome")?.InnerText;
+                    row["Destinatário"] = node.SelectSingleNode("dest/xNome")?.InnerText;
+
+                    var produto = node.SelectSingleNode("det/prod");
+                    row["Produto"] = produto?.SelectSingleNode("xProd")?.InnerText;
+                    row["Quantidade"] = produto?.SelectSingleNode("qCom")?.InnerText;
+
                     dataTable.Rows.Add(row);
                 }
 
                 using (var workbook = new XLWorkbook()) {
-                    var worksheet = workbook.Worksheets.Add(dataTable, "Dados");
+                    var worksheet = workbook.Worksheets.Add(dataTable, "NFe");
+
+                    worksheet.Cell(1, 1).Value = "Chave";
+                    worksheet.Cell(1, 2).Value = "Valor";
+                    
+                    for (int i = 0; i < dataTable.Rows.Count; i++) {
+                        worksheet.Cell(i + 2, 1).Value = dataTable.Rows[i]["Chave"]?.ToString();
+                        worksheet.Cell(i + 2, 1).Value = dataTable.Rows[i]["Valor"]?.ToString();
+                            }
+
+                    worksheet.Columns().AdjustToContents();
+
                     using (var stream = new System.IO.MemoryStream()) {
                         workbook.SaveAs(stream);
                         stream.Seek(0, System.IO.SeekOrigin.Begin);
