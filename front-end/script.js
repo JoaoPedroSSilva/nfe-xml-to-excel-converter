32240ac8a9f432fbee37c91cac17afe55de4afef
JoaoPedroSSilva <97jpss@gmail.com>
2025-01-22 13:07:54 -0300

    inclusão de barra de progresso
    inclusão de barra de progresso

--- front-end/script.js
+++ front-end/script.js
@@ -1,7 +1,6 @@
 document.getElementById('uploadForm').addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    console.log("Testando")
     const fileInput = document.getElementById('xmlFile');
     const file = fileInput.files[0];
 
@@ -20,8 +19,17 @@ document.getElementById('uploadForm').addEventListener('submit', async (event) =
         window.alert('Tamanho do arquivo excede o limite de 4MB!');
         return;
     }
+
+    uploadFile(file);
+});
+
+async function uploadFile(file) {
     const formData = new FormData();
     formData.append('file', file);
+
+    const progressContainer = document.getElementById('progress-container');
+    const progressBar = document.getElementById('progress-bar');
+    progressContainer.style.display = 'block';
 
     try {
         const response = await fetch('https://localhost:7075/api/NFe/convert', {
@@ -30,6 +38,8 @@ document.getElementById('uploadForm').addEventListener('submit', async (event) =
         });
 
         if (response.ok) {
+            progressBar.style.width = '100%';
+            progressBar.classList.add('bg-sucess');
             const blob = await response.blob();
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement('a');
@@ -39,10 +49,13 @@ document.getElementById('uploadForm').addEventListener('submit', async (event) =
             window.URL.revokeObjectURL(url);
             document.getElementById('status').textContent = 'Download concluído!';
         } else {
-            const errorText = await response.text();
-            document.getElementById('status').textContent = `Erro: ${errorText}`;
+            progressBar.classList.add('bg-danger');
+            document.getElementById('status').textContent = `Erro: ${await response.text()}`;
         }
     } catch (error) {
+        progressBar.classList.add('bg-danger');
         document.getElementById('status').textContent = `Erro de conexão: ${error.message}`;
+    } finally {
+        progressContainer.style.display = 'none';
     }
-});
+}
